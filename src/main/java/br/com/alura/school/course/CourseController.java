aa124e2b0c8f9f39d2d36da7ac7d83230f97a1e7
Artur Coutinho <arturpc@yahoo.com.br>
2021-12-10 19:03:29 -0300

    Corrigida listagem de cursos para passar CourseControllerTest. Adicionado /bin/ ao .gitignore.

--- src/main/java/br/com/alura/school/course/CourseController.java
+++ src/main/java/br/com/alura/school/course/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -22,7 +23,10 @@ class CourseController {
 
     @GetMapping("/courses")
     ResponseEntity<List<CourseResponse>> allCourses() {
-        return ResponseEntity.ok().build();
+    	List<Course> allCoursesList = courseRepository.findAll();
+    	List<CourseResponse> allCoursesResponse = new ArrayList<>();
+    	allCoursesList.forEach(course -> allCoursesResponse.add(new CourseResponse(course)));    	
+        return ResponseEntity.ok(allCoursesResponse);
     }
 
     @GetMapping("/courses/{code}")
--- src/main/java/br/com/alura/school/course/CourseRepository.java
+++ src/main/java/br/com/alura/school/course/CourseRepository.java
@@ -2,8 +2,11 @@ package br.com.alura.school.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 interface CourseRepository extends JpaRepository<Course, Long> {
     Optional<Course> findByCode(String code);
+    
+    List<Course> findAll();
 }
